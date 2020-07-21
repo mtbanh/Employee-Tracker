2d03f0fb7c537304cfed10ee63363df572e153a9
Mai Banh <65269951+mtbanh@users.noreply.github.com>
2020-07-21 16:54:07 -0700

    created cases for adding, viewing and editing data

--- server.js
+++ server.js
@@ -2,3 +2,99 @@ const mysql = require('mysql');
 const inquirer = require('inquirer');
 const consoleTable = require('console.table');
 const promisemysql = require('promise-mysql');
+
+const connectionProperties = {
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'password',
+    database: 'employees_DB'
+};
+
+const connection = mysql.createConnection(connectionProperties);
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log(`employee database`);
+});
+mainMenuPrompt()
+function mainMenuPrompt() {
+    let wantToCont = true;
+    while (wantToCont) {
+        inquirer.prompt({
+            name: 'userChoice',
+            type: 'list',
+            message: 'What would you like to do?',
+            choices: ['Add Departments', 'Add Roles', 'Add Employees', 'View Departments', 'View Roles', 'View Employees', 'Update Departments', 'Update Roles', 'Update Employees']
+        }).then((answer) => {
+            switch (answer.userChoice) {
+                case "Add Departments":
+                    addDept();
+                    break;
+
+                case "Add Roles":
+                    addRoles();
+                    break;
+
+                case "Add Employees":
+                    addEmp();
+                    break;
+
+                case "View Departments":
+                    viewDept();
+                    break;
+
+                case "View Roles":
+                    viewRoles();
+                    break;
+
+                case "View Employees":
+                    viewEmp();
+                    break;
+
+                case "Update Departments":
+                    updateDept();
+                    break;
+
+                case "Update Roles":
+                    updateRoles();
+                    break;
+
+                case "Update Employees":
+                    updateEmp();
+                    break;
+            }
+            const contEdit = await inquirer.prompt({
+                type: 'list',
+                name: 'continue',
+                message: 'Need to do more work?',
+                choices: ['Yes', 'No']
+            })
+
+            if(contEdit.continue === 'No'){
+                wantToCont = false;
+            }
+        })
+    }
+    
+};
+
+const addDept = () => {
+    inquirer.prompt({
+        name: 'deptName',
+        type: 'input',
+        message: "What's the department name?"
+    }).then((answer) => {
+        connection.query(`INSERT INTO department (name)VALUES ("${answer.deptName}");`, (err, res) => {
+            if (err) return err;
+            console.log(`Department added`);
+
+        })
+    })
+}
+
+const addRoles =()=>{
+    let DeptArr =[];
+
+}
+
