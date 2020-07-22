65ef2ef6c1edeada8e6cfacc85646c3360d19b95
Mai Banh <65269951+mtbanh@users.noreply.github.com>
2020-07-22 16:55:27 -0700

    able to view all employees

--- server.js
+++ server.js
@@ -285,8 +285,20 @@ function viewRoles() {
         })
     })
  };
-
-function viewEmp() { }
+//tested
+function viewEmp() {
+    let query = `SELECT e.id AS ID, e.first_name AS 'First Name', e.last_name AS 'Last Name', 
+    role.title AS Title, department.name AS Department, role.salary AS Salary, 
+    concat(m.first_name, ' ' ,  m.last_name) AS Manager FROM employee e 
+    LEFT JOIN employee m ON e.manager_id = m.id 
+    INNER JOIN role ON e.role_id = role.id 
+    INNER JOIN department ON role.department_id = department.id ORDER BY ID ASC`;
+    connection.query(query, (err, res)=>{
+        if (err) return err;
+        console.table(res);
+        mainMenuPrompt();
+    })
+ }
 
 function updateDept() { }
 
